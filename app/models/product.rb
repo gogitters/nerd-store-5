e9b80f8591764fd8270c11079b1a54cf3738c34a
Samantha Morco <samanthamorco@gmail.com>
2017-05-18 18:36:51 -0400

    makes database changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,22 @@
 class Product < ActiveRecord::Base
+
+  def sale_message 
+    if price <= 5
+      "Discount Item!!!!!!"
+    else
+      "Everyday Value!!!!!!!!"
+    end
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
+
+  def red_price
+    "sale_price" if price <= 5
+  end
 end
